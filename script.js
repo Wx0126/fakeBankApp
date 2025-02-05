97a7c30d52280869d95997d182f5baa3ae02211d
Xin Wang <94050546+Wx0126@users.noreply.github.com>
2025-02-05 12:39:06 +0000

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const account1 = {
 };
 
 const account2 = {
-  owner: 'Second User2',
+  owner: 'Dou Dou',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
